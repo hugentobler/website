8ed8222dda381bdd82a14bcb038859682e4795a0
Christopher Hugentobler <hugentoblerc@gmail.com>
2024-11-01 16:40:32 -0700

    Init

---
+++ svelte.config.js
@@ -0,0 +1,32 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), mdsvex()],
+
+	kit: {
+		// Cloudflare Pages adapter 
+		// https://svelte.dev/docs/kit/adapter-cloudflare
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
+	},
+
+	extensions: ['.svelte', '.svx']
+};
+
+export default config;
