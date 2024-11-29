878715b5859201a7656f2e3ef4eda5da5c2dfa7d
Christopher Hugentobler <hugentoblerc@gmail.com>
2024-11-29 08:21:21 -0800

    Test prerender build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,5 @@
-<script lang="ts">
-  import type { PageData } from "./$types";
-
-  let { data }: { data: PageData } = $props();
-</script>
-
 <h1>Welcome to Hugentobler</h1>
 <p>
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
   documentation
 </p>
-{#each data.posts as post}
-  <p>{post.title}</p>
-{/each}
