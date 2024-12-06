288676d30951edd7bef297a505aafda8874551bb
Christopher Hugentobler <hugentoblerc@gmail.com>
2024-12-06 09:33:09 -0800

    Make univers available

--- src/routes/typography/+page.svelte
+++ src/routes/typography/+page.svelte
@@ -8,14 +8,28 @@
 
 <p class="mb-4 font-mono font-thin">
   Lorem ipsum dolor sit amet, <em>consectetur adipiscing elit</em>. Sed do
-  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-  commodo consequat.
+  eiusmod tempor incididunt ut labore et dolore magna aliqua.
 </p>
 
 <p class="mb-4 font-sans font-bold">
   Lorem ipsum dolor sit amet, <em>consectetur adipiscing elit</em>. Sed do
-  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-  commodo consequat.
+  eiusmod tempor
+  <span class="font-light">incididunt ut labore et dolore magna aliqua</span>.
+</p>
+
+<p class="mb-4 font-sans font-stretch-expanded">
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+  aliquip ex ea commodo consequat.
+</p>
+
+<p class="mb-4 font-sans font-stretch-ultra-condensed">
+  <span class="font-thin">Lorem ipsum</span> dolor sit amet,
+  <em>consectetur adipiscing elit</em>. Sed do eiusmod tempor
+  <span class="font-light">incididunt ut labore et dolore magna aliqua</span>.
+</p>
+
+<p class="mb-4 font-sans font-stretch-condensed">
+  Lorem <b>ipsumdolor sit amet,</b> <em>consectetur adipiscing elit</em>. Sed do
+  eiusmod tempor
+  <span class="font-light">incididunt ut labore et dolore magna aliqua</span>.
 </p>
