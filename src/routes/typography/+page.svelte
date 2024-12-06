372837f1c493eb9172e1ee420e9c13937f8f556b
Christopher Hugentobler <hugentoblerc@gmail.com>
2024-12-05 21:57:48 -0800

    Fix fonts

--- src/routes/typography/+page.svelte
+++ src/routes/typography/+page.svelte
@@ -2,10 +2,13 @@
 
 <h2 class="text-3xl font-mono font-light mb-3">Jumps Over The Lazy Dog</h2>
 
-<h3 class="text-2xl font-mono mb-2">While The World Watches</h3>
+<h3 class="text-2xl font-mono font-black mb-2">
+  <em>While The World -> Watches</em>
+</h3>
 
-<p class="mb-4 font-mono">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+<p class="mb-4 font-mono font-thin">
+  Lorem ipsum dolor sit amet, <em>consectetur adipiscing elit</em>. Sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+  commodo consequat.
 </p>
