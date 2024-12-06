3d0f8876f36adebfc61001409246795f86e42c4b
Christopher Hugentobler <hugentoblerc@gmail.com>
2024-12-05 22:10:47 -0800

    Add Univers

--- src/routes/typography/+page.svelte
+++ src/routes/typography/+page.svelte
@@ -12,3 +12,10 @@
   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
   commodo consequat.
 </p>
+
+<p class="mb-4 font-sans font-bold">
+  Lorem ipsum dolor sit amet, <em>consectetur adipiscing elit</em>. Sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+  commodo consequat.
+</p>
