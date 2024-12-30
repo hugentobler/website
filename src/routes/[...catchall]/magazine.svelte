3c67824b0898d8211629dd621aefe63c278a53d5
Christopher Hugentobler <hugentoblerc@gmail.com>
2024-12-29 22:04:04 -0800

    Add magazine styling

--- src/routes/[...catchall]/magazine.svelte
+++ src/routes/[...catchall]/magazine.svelte
@@ -4,7 +4,18 @@
   let { children, title, updated } = $props() as MarkdownLayoutProps;
 </script>
 
-<article class="@container prose max-w-none">
+<article
+  class="
+  scroll-timeline-y md:scroll-timeline-x relative grid h-full w-full
+       grid-cols-11 gap-y-8 overflow-x-hidden
+       overflow-y-scroll scroll-smooth [orphans:1] md:block
+       md:gap-x-8 md:overflow-x-scroll md:overflow-y-hidden md:scroll-auto
+       md:py-8
+       md:[column-width:calc((99vw-6rem)/2)]
+       lg:[column-width:calc((99vw-8rem)/3)]
+       2xl:[column-width:calc(80rem/3)]
+"
+>
   <header>
     <h1>
       {title}
