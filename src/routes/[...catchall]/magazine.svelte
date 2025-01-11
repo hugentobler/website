ce2c0ebb05ca748fd21bc7d60e23503c2cae81f6
Christopher Hugentobler <hugentoblerc@gmail.com>
2025-01-10 19:49:11 -0800

    Added scrollbars to magazine

--- src/routes/[...catchall]/magazine.svelte
+++ src/routes/[...catchall]/magazine.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
   import type { MarkdownLayoutProps } from '$lib/types';
+  import { onMount } from 'svelte';
 
   let { children, title, updated } = $props() as MarkdownLayoutProps;
+  let article: HTMLElement;
+
+  onMount(() => {
+    // Horizontal scroll listener
+    const handleWheel = (event: WheelEvent) => {
+      event.preventDefault();
+      article.scrollLeft += event.deltaY + event.deltaX;
+    };
+
+    // Observe article scroll style to add/remove horizontal scroll listener
+    const observer = new ResizeObserver(() => {
+      const overflowX = window.getComputedStyle(article).overflowX;
+      if (overflowX === 'scroll') {
+        window.addEventListener('wheel', handleWheel, { passive: false });
+      } else {
+        window.removeEventListener('wheel', handleWheel);
+      }
+    });
+    observer.observe(article);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('wheel', handleWheel);
+      observer.disconnect();
+    };
+  });
 </script>
 
-<article>
+<article bind:this={article}>
   <header>
     <h1>
       {title}
@@ -34,20 +61,25 @@
     widows: 2;
 
     /* Columns */
-    @media (width >= theme(--breakpoint-md)) {
+    @media (width >= theme(--breakpoint-lg)) {
       --visible-columns: 2;
       column-gap: theme('spacing.8');
+      /* container query width relies on parent container class */
       columns: calc(
-        (100cqw - (theme('spacing.8') * 2)) / var(--visible-columns)
-      ); /* Parent container class required */
+        (100cqw - (theme('spacing.8') * (var(--visible-columns) - 1))) / var(--visible-columns)
+      );
     }
 
-    @media (width >= theme(--breakpoint-lg)) {
+    @media (width >= theme(--breakpoint-xl)) {
       --visible-columns: 3;
     }
 
+    @media (width >= theme(--breakpoint-2xl)) {
+      --visible-columns: 4;
+    }
+
     /* Scrollbars */
-    @apply overflow-x-hidden overflow-y-scroll md:overflow-x-scroll md:overflow-y-hidden;
-    @apply scroll-smooth md:scroll-auto;
+    @apply overflow-x-hidden overflow-y-scroll lg:overflow-x-scroll lg:overflow-y-hidden;
+    @apply scroll-smooth lg:scroll-auto;
   }
 </style>
