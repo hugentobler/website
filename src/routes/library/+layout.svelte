cdd3924d97bcf527d88a037caad7e6f730f31259
Christopher Hugentobler <hugentoblerc@gmail.com>
2025-08-03 19:11:36 -0700

    Start library layout

---
+++ src/routes/library/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { LayoutProps } from "./$types";
+
+  let { children }: LayoutProps = $props();
+</script>
+
+<nav class="grid grid-cols-[2fr_1fr_1fr] gap-4">
+  <h1>My Library</h1>
+  <ul>
+    <li>Home</li>
+    <li>Resume</li>
+    <li>Typography</li>
+  </ul>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+</nav>
+
+<main>
+  {@render children()}
+</main>
+
+<style lang="postcss">
+  @reference "../../app.css";
+
+  nav {
+    @apply border-b border-b-black;
+  }
+</style>
